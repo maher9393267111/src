0942479853f1dfff33fe3352f3ae9f6f67bd843d
maher <gomemahero.com>
2022-05-07 15:22:31 +0300

    chakura provider is work

--- pages/practice/index.js
+++ pages/practice/index.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import { motion,useMotionValue,useTransform } from 'framer-motion';
+import React from "react";
+
+
+// Import Swiper styles
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/virtual';
+import {   Box,
+  Flex,
+  HStack,
+  Link,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Heading,
+  Text,
+  Image,
+  VStack,
+  Center,
+ } from '@chakra-ui/react'
+import { motion, useMotionValue, useTransform } from "framer-motion";
 const Practice = () => {
 
-    const x = useMotionValue(0)
-    const opacity = useTransform(x, [-10, 0, 100], [0, 2, 0])
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  
 
 
 
-    return (
-        <div>
-            <h1>Practice</h1>
+  return (
+    <div>
+      <h1 className="   bg-slate-500">Practice</h1>
+     
+      <Center bg='tomato' h='100px' color='white'>
+  This is the Center
+</Center>
+      <Button onClick={onOpen}>Open Drawer</Button>
+      <Drawer placement='right' onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
 
-            <motion.div animate={{ scale: 0.5 }} >
-<h1>motition hello</h1>
-
-
-            </motion.div>
 
 
 
 
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Practice;
